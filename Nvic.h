d49d5a696e915449537fda57ab5bd332020ef04c
7arby95 <youssefharby96@gmail.com>
2020-04-17 21:28:24 +0200

    Task1 added (NVIC, SysCtl and Privilege switching)

---
+++ Nvic.h
@@ -0,0 +1,20 @@
+/*
+ * NVIC.h
+ *
+ *  Created on: Apr 8, 2020
+ *      Author: Youssef Harby
+ */
+
+
+#ifndef NVIC_H_
+#define NVIC_H_
+
+
+/********************************************************************************
+ * 								API's Declarations								*
+ ********************************************************************************/
+
+extern void NVIC_Init(void);
+
+
+#endif /* NVIC_H_ */
